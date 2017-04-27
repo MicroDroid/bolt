cf506e131d0b733652dc334a34e79cc40f0b00e3
Yousef Sultan <yousef.su.2000@gmail.com>
2017-04-27 03:16:45 +0300

    Add inspire.js

---
+++ plugins/inspire.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+
+module.exports = {
+	handle: (message, event, bot) => {
+		axios.get('http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json&key=' + message.args)
+			.then(response => {
+				bot.postMessage(event.channel, `${response.data.quoteAuthor} says: ${response.data.quoteText}`);
+			}).catch(error => {
+				bot.postMessage(event.channel, 'API seems down');
+			})
+	}
+}
