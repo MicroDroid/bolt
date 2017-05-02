e2adb66ed3a72eae035147a14a0d2904cd636bb8
Yousef Sultan <yousef.su.2000@gmail.com>
2017-05-02 15:18:17 +0300

    ew

--- plugins/remind.js
+++ plugins/remind.js
@@ -6,10 +6,10 @@ module.exports = {
 	description: 'Reminds you or someone else to do stuff',
 
 	handle: (message, event, datastore, bot) => {
-		const matches = (/(me|<@([\w\d]+)>)\s+(to\s+)*(.+)\s+in\s+([\s\d\w]+)/).exec(message.args);
+		const matches = (/(me|<@([\w\d]+)>)\s+(to\s+)?(.+)\s+in\s+([\s\d\w]+)/).exec(message.args);
 
 		if (!matches)
-			return bot.postMessage(event.channel, `Syntax is:\n\`\`\`{process.env.PREFIX} remind <user> [to] do stuff... <in time>\`\`\``);
+			return bot.postMessage(event.channel, `Syntax is:\n\`\`\`${process.env.PREFIX}remind <user> [to] do stuff... <in time>\`\`\``);
 
 		const user = matches[1] === 'me' ? event.user : matches[2];
 		const reminder = matches[4];
@@ -24,6 +24,5 @@ module.exports = {
 		}, reltime.parse(new Date(), target) - Date.now());
 		
 		bot._api('reactions.add', {name: 'thumbsup', channel: 'C54UQ9N8M', timestamp: event.ts});
-        // bot.postMessage(event.channel, ':thumbsup::skin-tone-3:');
 	}
 }
